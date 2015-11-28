76820d638c3207b1d87ffaff24d00aa411da25bb
tuta <dcavalcanticosta@gmail.com>
2015-11-28 16:15:42 -0200

    fibonacci_closure

---
+++ fibonacci_closure/fibonacci_closure.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	x := 0
+	y := 1
+	return func() int{
+		fmt.Printf("x, y = %d, %d \n", y, y + x)
+		x, y = y, x + y
+		return y
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
