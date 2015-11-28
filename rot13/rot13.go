3572fc949646e7f011594eb8d93d615ba3c432b9
tuta <dcavalcanticosta@gmail.com>
2015-11-28 20:25:25 -0200

    reader e rot13

---
+++ rot13/rot13.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+
+func (r *rot13Reader) Read(b []byte) (n int, err error){
+	n, err = r.r.Read(b)
+	for i := 0; i < len(b); i++ {
+		if (b[i] >= 'A' && b[i] <= 'M') || (b[i] >= 'a' && b[i] <= 'm') {
+			b[i] += 13
+		}else if (b[i] >= 'N' && b[i] <= 'Z') || (b[i] >= 'n' && b[i] <= 'z') {
+			b[i] -= 13
+		}
+	}
+	return
+}
+
+
+
+
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
